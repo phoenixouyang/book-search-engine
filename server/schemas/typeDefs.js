d7cbcba6de574365f37709114cde68aa63b1b54d
Pholobo <ouyangphoenix@gmail.com>
2023-05-20 19:10:58 -0400

    updates mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,7 +39,7 @@ const typeDefs = gql`
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        saveBook:(newBook: saveBookInput): User
+        saveBook:(newBook: saveBookInput!): User
         removeBook: (bookId: ID!): User
     }
 `;
